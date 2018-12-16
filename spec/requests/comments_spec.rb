3b40cc3c6ad4db87c820778e2a640ad5ed8efb52
Nikitin Egor <negor.dev@gmail.com>
2018-12-16 20:01:25 +0300

    TIL-002 Added request test for creating comment

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -7,9 +7,9 @@ EMPTY_ARRAY_LENGTH = 0.freeze
 RSpec.describe Comment, type: :request, broken: true do
   let(:comment) {create(:comment)}
   let!(:user) {create(:user)}
-  let!(:post) {create(:post)}
+  let!(:one_post) {create(:post)}
   let!(:post_without_comments) {create(:post)}
-  let!(:comments) {create_list(:comment, POST_COMMENTS, post: post)}
+  let!(:comments) {create_list(:comment, POST_COMMENTS, post: one_post)}
 
   before do
     post '/api/auth',
@@ -20,9 +20,9 @@ RSpec.describe Comment, type: :request, broken: true do
     @token = json['auth_token']
   end
 
-  describe "GET /api/posts/:id/comments" do
+  describe 'GET /api/posts/:id/comments' do
     context 'when post has comments' do
-      before {get "/api/posts/#{post.id}/comments"}
+      before {get "/api/posts/#{one_post.id}/comments"}
 
       it 'returns all comments of post as json' do
         expect(json.length).to eq(POST_COMMENTS)
@@ -47,6 +47,10 @@ RSpec.describe Comment, type: :request, broken: true do
     context 'when post doesn\'t exists' do
       before {get "/api/posts/0/comments"}
 
+      it 'returns correct error message' do
+        expect(json['message']).to eq(['Post Not Found'])
+      end
+
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
@@ -55,7 +59,7 @@ RSpec.describe Comment, type: :request, broken: true do
 
   describe "GET /api/posts/:id/comments?limit=#{COMMENTS_BY_PAGE}&offset=0" do
     context 'when post has comments' do
-      before {get "/api/posts/#{post.id}/comments?limit=#{COMMENTS_BY_PAGE}&offset=0"}
+      before {get "/api/posts/#{one_post.id}/comments?limit=#{COMMENTS_BY_PAGE}&offset=0"}
 
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
@@ -66,7 +70,7 @@ RSpec.describe Comment, type: :request, broken: true do
       end
     end
     context 'when post hasn\'t comments' do
-      before {get "/api/posts/#{post_without_comments.id}/comments?limit=#{COMMENTS_BY_PAGE}&offset=0"}
+      before {post "/api/posts/#{post_without_comments.id}/comments?limit=#{COMMENTS_BY_PAGE}&offset=0"}
 
       it 'returns empty array' do
         expect(json.length).to eq(EMPTY_ARRAY_LENGTH)
@@ -74,6 +78,58 @@ RSpec.describe Comment, type: :request, broken: true do
 
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe "POST /api/posts/:id/comment" do
+    let(:valid_attributes) {{text: Faker::Lorem.sentence}}
+    let(:invalid_attributes) {{text: Faker::Lorem.paragraph_by_chars 600}}
+    context 'when comment attributes is correct' do
+      before {post "/api/posts/#{one_post.id}/comment", params: valid_attributes, headers: {Authorization: @token}}
+
+      it 'returns saved comment' do
+        expect(json).not_to be_empty
+      end
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+
+    context 'when add comment without auth_token' do
+      before {post "/api/posts/#{one_post.id}/comments", params: valid_attributes}
+
+      it 'returns correct error message' do
+        expect(json['message']['token']).to eq(['Missing token', 'Invalid token'])
+      end
+
+      it 'returns status code 401' do
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context 'when try add comment to not existing post' do
+      before {post '/api/posts/0/comments', params: valid_attributes, headers: {Authorization: @token}}
+
+      it 'returns correct error message' do
+        expect(json['message']).to eq(['Post Not Found'])
+      end
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context 'when try add comment with invalid params' do
+      before {post "/api/posts/#{one_post.id}/comments", params: invalid_attributes, headers: {Authorization: @token} }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns correct error message' do
+        expect(json['message']['text']).to eq(['Invalid text'])
       end
     end
   end
